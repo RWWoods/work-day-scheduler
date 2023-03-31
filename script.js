e3358986197191c513bb59c905cefacf6e68138d
Kiggles <ridgewwoods@gmail.com>
2023-03-30 23:37:29 -0400

    added comments to script.js

--- script.js
+++ script.js
@@ -22,12 +22,14 @@
 // TODO: Add code to display the current date in the header of the page.
 
 $(function () {
+  // these variables set the date at the top of the page to todays date
   var date = document.getElementById("currentDay");
-  var todayDate = dayjs().format('dddd, MMMM DD, YYYY')
+  var todayDate = dayjs().format('dddd MMMM DD, YYYY')
   date.textContent = todayDate;
 
   var timeHeader = document.getElementById("currentTime");
 
+  // the following function sets an interval of 1 second to update the clock at the top of the page
   function displayTime() {
     var now = dayjs().format(' hh:mm:ss a')
     timeHeader.textContent = "It is currently " + now;
@@ -36,18 +38,18 @@ $(function () {
   displayTime();
   setInterval(displayTime, 1000);
 
+  // the following variables are used to adjust the color of the time blocks according to the current time.
   var hourBlocks = document.getElementsByClassName("row time-block");
-  // var currentHour = dayjs().format('H');
-  var currentHour = 13
+  var currentHour = dayjs().format('H');
   var hourArray = [9, 10, 11, 12, 13, 14, 15, 16, 17]
   var x = -1;
 
   var containerEl = $(".container-lg");
-
+// these variables create a blank array that can be updated with saved data from local storage
   var existingEntries = JSON.parse(localStorage.getItem("allEntries"));
   if (existingEntries == null) existingEntries = [];
-  console.log(hourBlocks[0].children[1]);
 
+// this on click function saves data from the corrosponding text area to a local storage array
   containerEl.on("click", ".saveBtn", function (event) {
     var allEntries = []
     entryText = $(event.target).siblings().eq(1).val();
@@ -55,8 +57,8 @@ $(function () {
     existingEntries.push(entryText);
     localStorage.setItem("allEntries", JSON.stringify(existingEntries))
   })
-
-  for (let i = 0; i < hourBlocks.length; i++) {
+// this for loop is what sets the color of the hourblocks by modifying the class based on current time
+  for (var i = 0; i < hourBlocks.length; i++) {
 
     x++
     if (currentHour > hourArray[x]) {
@@ -67,9 +69,9 @@ $(function () {
       hourBlocks[x].setAttribute("class", "row time-block future");
     }
   }
-
+// this for loop iterates through the hourBlocks and pulls data from the local storage array to fill them on page load... the order of the data might need work.
   for (let k = 0; k < hourBlocks.length; k++) {
-    // y++
+
     if (existingEntries[k] != null) {
       hourBlocks[k].children[1].innerHTML = existingEntries[k];
     } else {
